1b0c52abfffddc45227fe74ae8535e781d2759b9
euro20179 <sandollar2020@gmail.com>
2023-10-12 14:53:10 -0700

    code: add way to request for user information

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 local _M =  require"discord._discord"
 _M.channels = require"discord.resources.channels"
 _M.servers = require"discord.resources.servers"
+_M.users = require"discord.resources.users"
 return _M
---
+++ resources/users.lua
@@ -0,0 +1,38 @@
+---@class discord.User
+---@field username string
+---@field discriminator string
+---@field global_name string?
+---@field avatar string?
+---@field bot boolean?
+---@field system boolean?
+---@field mfa_enabled boolean?
+---@field banner string?
+---@field accent_color integer?
+---@field locale string?
+---@field verified boolean?
+---@field email string?
+---@field flags integer?
+---@field premium_type integer?
+---@field public_flags integer?
+---@field avatar_decoration string?
+
+local config = require"discord.config"
+local cache = require"discord.cache"
+
+local _M = {}
+
+---@param user_id string
+---@return discord.User?
+_M.get_user = function(user_id)
+    if cache[user_id] then
+        return cache[user_id]
+    end
+    local out = vim.system({ "curl", "-H", "Authorization: " .. config.token,
+        "https://discord.com/api/v10/users/" .. user_id }):wait()
+    if out.code == 0 then
+        cache[user_id] = vim.json.decode(out.stdout)
+        return cache[user_id]
+    end
+end
+
+return _M
